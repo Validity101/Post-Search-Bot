bb133a8eaa082928a575b3a27e6f35d10c707b12
Rishi <dramawave7@gmail.com>
2025-07-01 16:30:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = 24890303
+API_HASH     = "94cf78d1e6883ecb10f32e31fc23cfe0"
 BOT_TOKEN    = ""
 SESSION      = ""
 DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "codeflix_bots"
+LOG_CHANNEL  = -1002664392843
+ADMIN        = 7313000783
+CHANNEL      = "KdramaTalkies"
